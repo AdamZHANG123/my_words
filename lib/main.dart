239553e39f37e7a983cf726589a2ab8ddcaace23
Adam Zhang <adamisagoodguy@gmail.com>
2019-05-08 19:46:42 +0800

    Part 2 finished
    1. Added Icon to trailing of ListTile.
    2. Added IconButton to actions of AppBar.
    3. Added Another page called LikedSuggestions, which displays the suggestions we liked in the first page.
    4. Using Navigator to push to new Page and back.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
+final _biggerFont = const TextStyle(fontSize: 18.0);
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -20,7 +22,7 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
+  Set<WordPair> _savedSuggestions = Set<WordPair>();
 
   @override
   Widget build(BuildContext context) {
@@ -28,6 +30,20 @@ class _RandomWordsState extends State<RandomWords> {
       appBar: AppBar(
         centerTitle: true,
         title: Text('Startup Name Generator'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => LikedSuggestions(
+                          saved: _savedSuggestions,
+                        )),
+              );
+            },
+          )
+        ],
       ),
       body: _buildSuggestions(),
     );
@@ -47,12 +63,56 @@ class _RandomWordsState extends State<RandomWords> {
     );
   }
 
-  Widget _buildRow(WordPair wordPair) {
+  Widget _buildRow(WordPair pair) {
+    final _savedAlready = _savedSuggestions.contains(pair);
+
     return ListTile(
       title: Text(
-        wordPair.asPascalCase,
+        pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        _savedAlready ? Icons.favorite : Icons.favorite_border,
+        color: _savedAlready ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          _savedAlready
+              ? _savedSuggestions.remove(pair)
+              : _savedSuggestions.add(pair);
+          print(_savedSuggestions);
+        });
+      },
     );
+  }
+}
+
+class LikedSuggestions extends StatelessWidget {
+  final Set<WordPair> saved;
+
+  LikedSuggestions({Key key, this.saved}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final tiles = saved.map((WordPair pair) {
+      return ListTile(
+        title: Text(
+          pair.asPascalCase,
+          style: _biggerFont,
+        ),
+      );
+    });
+
+    final divided =
+        ListTile.divideTiles(context: context, tiles: tiles).toList();
+
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Liked Suggestions'),
+        ),
+        body: ListView(
+          children: divided,
+        ));
   }
 }
